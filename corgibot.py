48ed480d1606a446b0b58ec1b0c6e12b09d2e8c2
Thom <larnu@gmail.com>
2020-06-17 10:50:23 +0100

    All functions apart from timers

--- corgibot.py
+++ corgibot.py
@@ -7,12 +7,14 @@ from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
 
-client = discord.Client()
-
-directory = os.path.dirname(os.path.realpath(__file__))
-
 load_dotenv()
 key = os.getenv('DISCORD_KEY')
+
+#Bad way to do it, I know.
+colours = ["default","teal","dark teal","green","dark green","blue","dark blue","purple","dark purple","magenta","dark magenta","gold","dark gold","orange","dark orange","red","dark red","lighter grey", "dark grey", "light grey", "darker grey", "blurple", "greyple"]
+colourings = {"default":discord.Colour.default(),"teal":discord.Colour.teal(),"dark teal":discord.Colour.dark_teal(),"green":discord.Colour.green(),"dark green":discord.Colour.dark_green(),"blue":discord.Colour.blue(),"dark blue":discord.Colour.dark_blue(),"purple":discord.Colour.purple(),"dark purple":discord.Colour.dark_purple(),"magenta":discord.Colour.magenta(),"dark magenta":discord.Colour.dark_magenta(),"gold":discord.Colour.gold(),"dark gold":discord.Colour.dark_gold(),"orange":discord.Colour.orange(),"dark orange":discord.Colour.dark_orange(),"red":discord.Colour.red(),"dark red":discord.Colour.dark_red(),"lighter grey":discord.Colour.lighter_grey(),"dark grey":discord.Colour.dark_grey(),"light grey":discord.Colour.light_grey(),"darker grey":discord.Colour.darker_grey(),"blurple":discord.Colour.blurple(),"greyple":discord.Colour.greyple()}
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -33,9 +35,21 @@ async def on_message(message):
             logchannel = channel
             break
 
+    for mentions in message.mentions:
+        if mentions.id == message.guild.me.id:
+            await message.channel.send("Woof?")
+
     if message.content.lower() == ("$roll"):
         i = random.randint(1,6)
         response = "You rolled a " + str(i) + " {0.author.mention}."
+        await message.channel.send(response.format(message))
+
+    if message.content.lower() == ("$flip"):
+        i = random.randint(0,1)
+        if i == 0:
+            response = "It's Heads!"
+        else:
+            response = "It's Tails!"
         await message.channel.send(response.format(message))
 
     #Want these commands in the right channel
@@ -48,6 +62,11 @@ async def on_message(message):
                 isCommittee = True
             if role.name == "Team Captain":
                 isCaptain = True
+
+        if message.content.lower() == "$colours":
+            response = "You can choose from any of the following colours: *" + ", ".join(colours) + "*"
+            await message.channel.send(response.format(message))
+            return
 
         #Give someone the Team Captain Rank
         if message.content.lower().startswith("$addcaptain"):
@@ -65,6 +84,17 @@ async def on_message(message):
                 await message.mentions[0].add_roles(role)
                 response = "{0.mentions[0].display_name} has been given the Team Captain Role."
                 await message.channel.send(response.format(message))
+                dm = """Hi, you've been given the Team Captain role in the Warmachine and Hordes Virtual Team Championship!
+
+To get started, you'll want to create your team using the `$teamname` command in the bot-commands channel. Once you've done that, you'll have access to all the table channels, and your team will have it's own channel category created. If you''re not completely decided on a name yet, don't worry, you can change it again later using the same command.
+
+Once your team has been created, you can start adding people to the team using the `$addplayer` command. You can also choose the colour of your team's role using $teamcolour.
+
+If you get stuck, just visit the guide in the #bot-help channel: <https://discordapp.com/channels/721685559277256806/721693621413478420/722049487874424892>
+
+Thanks for joining the tournament, and good luck!"""
+                newcaptain = client.get_user(message.mentions[0].id)
+                await newcaptain.send(dm.format(message))
                 #Log details
                 embed = discord.Embed(title="Add Team Captain", color=0xa551be) #description=message.mentions[0].display_name
                 embed.add_field(name="Added By", value=message.author.display_name, inline=False)
@@ -80,7 +110,7 @@ async def on_message(message):
                 await message.channel.send("Only Committee members can use that command.")
                 return
             elif len(message.mentions) == 0:
-                await message.channel.send("Needs a user to give JUdge role.")
+                await message.channel.send("Needs a user to give Judge role.")
                 return
             elif len(message.mentions[0].roles) > 1:
                 await message.channel.send("User already has a server role and cannot be made a Judge.")
@@ -91,13 +121,14 @@ async def on_message(message):
                 response = "{0.mentions[0].display_name} has been given the Judge Role."
                 await message.channel.send(response.format(message))
                 #Log details
-                embed = discord.Embed(title="Add Judge", color=0x0033dd) #description=message.mentions[0].display_name
+                embed = discord.Embed(title="Add Judge", color=discord.Colour.blue()) #description=message.mentions[0].display_name
                 embed.add_field(name="Added By", value=message.author.display_name, inline=False)
                 embed.add_field(name="Added ID", value=message.author.id, inline=False)
                 embed.add_field(name="Judge Added", value=message.mentions[0].display_name, inline=False)
                 embed.add_field(name="Judge ID", value=message.mentions[0].id, inline=False)
                 await logchannel.send(embed=embed)
                 return
+
 
         #Give someone the Judge Rank
         if message.content.lower().startswith("$addheadjudge"):
@@ -118,12 +149,72 @@ async def on_message(message):
                 response = "{0.mentions[0].display_name} has been given the Head Judge Role."
                 await message.channel.send(response.format(message))
                 #Log details
-                embed = discord.Embed(title="Add Head Judge", color=0x001199) #description=message.mentions[0].display_name
+                embed = discord.Embed(title="Add Head Judge", color=discord.Colour.dark_blue()) #description=message.mentions[0].display_name
                 embed.add_field(name="Added By", value=message.author.display_name, inline=False)
                 embed.add_field(name="Added ID", value=message.author.id, inline=False)
                 embed.add_field(name="Head Judge Added", value=message.mentions[0].display_name, inline=False)
                 embed.add_field(name="Head Judge ID", value=message.mentions[0].id, inline=False)
                 await logchannel.send(embed=embed)
+                return
+
+        if message.content.lower().startswith("$removejudge"):
+            if isCommittee == False:
+                await message.channel.send("Only Committee members can use that command.")
+                return
+            elif len(message.mentions) == 0:
+                await message.channel.send("Needs a user to remove Streamer role.")
+                return
+            else:
+                if len(message.mentions[0].roles) < 2:
+                    await message.channel.send("User does not have the Judge Role to remove.")
+                elif len(message.mentions[0].roles) == 2:
+                    role = message.mentions[0].roles[1]
+                    if role.name == "Judge":
+                        await message.mentions[0].remove_roles(role)
+                        response = "{0.mentions[0].display_name} has been removed from the Judge Role."
+                        await message.channel.send(response.format(message))
+                        #Log details
+                        embed = discord.Embed(title="Remove Player as Judge", color=discord.Colour.blue()) 
+                        embed.add_field(name="Removed By", value=message.author.display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.author.id, inline=False)
+                        embed.add_field(name="Removed Player", value=message.mentions[0].display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.mentions[0].id, inline=False)
+                        await logchannel.send(embed=embed)
+                    else:
+                        await message.channel.send("User does not have the Judge Role to remove.")
+                else:
+                    role1 = message.mentions[0].roles[1]
+                    role2 = message.mentions[0].roles[2]
+                    if role2.name == "Judge" or role2.name == "Head Judge":
+                        await message.mentions[0].remove_roles(role2)
+                        response = "{0.mentions[0].display_name} has been removed from the " + role2.name + " role."
+                        await message.channel.send(response.format(message))
+                        if role2.name == "Judge":
+                            colour = discord.Colour.blue()
+                        else:
+                            colour = discord.Colour.dark_blue()
+                        #Log details
+                        embed = discord.Embed(title="Remove Player as " + role2.name, color=colour) 
+                        embed.add_field(name="Removed By", value=message.author.display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.author.id, inline=False)
+                        embed.add_field(name="Removed Player", value=message.mentions[0].display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.mentions[0].id, inline=False)
+                        await logchannel.send(embed=embed)
+                    if role1.name == "Judge" or role1.name == "Head Judge":
+                        await message.mentions[0].remove_roles(role1)
+                        response = "{0.mentions[0].display_name} has been removed from the " + role1.name + " role."
+                        await message.channel.send(response.format(message))
+                        if role1.name == "Judge":
+                            colour = discord.Colour.blue()
+                        else:
+                            colour = discord.Colour.dark_blue()
+                        #Log details
+                        embed = discord.Embed(title="Remove Player as " + role1.name, color=colour) 
+                        embed.add_field(name="Removed By", value=message.author.display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.author.id, inline=False)
+                        embed.add_field(name="Removed Player", value=message.mentions[0].display_name, inline=False)
+                        embed.add_field(name="Removed ID", value=message.mentions[0].id, inline=False)
+                        await logchannel.send(embed=embed)
                 return
 
         #Give someone the Judge Rank
@@ -151,6 +242,29 @@ async def on_message(message):
                 await logchannel.send(embed=embed)
                 return
 
+        if message.content.lower().startswith("$removestreamer"):
+            if isCommittee == False:
+                await message.channel.send("Only Committee members can use that command.")
+                return
+            elif len(message.mentions) == 0:
+                await message.channel.send("Needs a user to remove Streamer role.")
+                return
+            else:
+                role = message.mentions[0].roles[1]
+                if role.name == "Streamer":
+                    await message.mentions[0].remove_roles(role)
+                    response = "{0.mentions[0].display_name} has been removed from the Streamer Role."
+                    await message.channel.send(response.format(message))
+                    #Log details
+                    embed = discord.Embed(title="Remove Player as Streamer", color=0x9147ff) 
+                    embed.add_field(name="Removed By", value=message.author.display_name, inline=False)
+                    embed.add_field(name="Removed ID", value=message.author.id, inline=False)
+                    embed.add_field(name="Removed Player", value=message.mentions[0].display_name, inline=False)
+                    embed.add_field(name="Removed ID", value=message.mentions[0].id, inline=False)
+                    await logchannel.send(embed=embed)
+                else:
+                    await message.channel.send("User does not have the Streamer Role to remove.")
+                return
 
         if message.content.lower().startswith("$teamname"):
             if isCaptain == False:
@@ -174,8 +288,6 @@ async def on_message(message):
                         HeadJudgeRole = guildrole
                 #Check Team Captain is already in a Team
                 if len(message.author.roles) == 2:
-                    response = "Creating team " + teamname
-                    await message.channel.send(response)
                     newrole = await guild.create_role(name=teamname, hoist=True)
                     await newrole.edit(position=2) 
                     await CaptainRole.edit(position=1)
@@ -197,16 +309,18 @@ async def on_message(message):
                     await message.channel.send("Table Permissions set. Creating team channels.")
                     #Create the Category and Channels
                     newcategory = await guild.create_category(teamname)
+                    await newcategory.edit(position=5)
                     talkrights = {guild.default_role: discord.PermissionOverwrite(read_messages=False),guild.me: discord.PermissionOverwrite(read_messages=True)}
-                    newtalk = await guild.create_text_channel(name="Team Talk",category=newcategory,overwrites=talkrights)
+                    newtalk = await guild.create_text_channel(name="Team Chat",category=newcategory,overwrites=talkrights)
                     await newtalk.set_permissions(newrole, read_messages=True, send_messages=True, embed_links=True, attach_files=True, read_message_history=True, use_external_emojis=True)
                     await newtalk.set_permissions(CommitteeRole, read_messages=True, send_messages=True, embed_links=True, attach_files=True, read_message_history=True, use_external_emojis=True)
                     await newtalk.set_permissions(HeadJudgeRole, read_messages=True, send_messages=True, embed_links=True, attach_files=True, read_message_history=True, use_external_emojis=True)
                     chatrights = {guild.default_role: discord.PermissionOverwrite(view_channel=False)}
-                    newchat = await guild.create_voice_channel(name="Team Chat",category=newcategory,overwrites=chatrights)
+                    newchat = await guild.create_voice_channel(name="Team Talk",category=newcategory,overwrites=chatrights)
                     await newchat.set_permissions(newrole,view_channel=True,connect=True,speak=True)
                     await newchat.set_permissions(CommitteeRole,view_channel=True,connect=True,speak=True)
                     await newchat.set_permissions(HeadJudgeRole,view_channel=True,connect=True,speak=True)
+                    await message.channel.send("New Team created.")
                     #Log details
                     embed = discord.Embed(title="Create New Team", color=0x999999) 
                     embed.add_field(name="Created By", value=message.author.display_name, inline=False)
@@ -244,17 +358,25 @@ async def on_message(message):
                 teamcolour = message.content[12:].lower()
                 if len(message.author.roles) == 2:
                     await message.channel.send("You need to have created your team before you can set their colour.")
-                elif not re.match("[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]",message.content[12:].lower()):
-                    await message.channel.send("Invalid hex colour.")
+                elif message.content.lower() == "$teamcolour":
+                    await message.channel.send("You must supply a colour. Either provide a colour hex, or use one of the preprogrammed colours. See `$colours` or <https://htmlcolorcodes.com/color-picker/> for ideas.")
+                elif not re.match("[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]",teamcolour) and colours.count(teamcolour) == 0:
+                    await message.channel.send("Invalid hex or predefined colour.")
                 else:
                     if message.author.roles[1].name == "Team Captain":
                         teamrole = message.author.roles[2]
                     else:
                         teamrole = message.author.roles[1]
-                    await teamrole.edit(colour=discord.Colour(int("0x"+teamcolour,16)))
+                    if colours.count(teamcolour) == 1:
+                        colour = colourings[teamcolour]
+                        await teamrole.edit(colour=discord.Colour(colour.value))
+                        embed = discord.Embed(title="Change Team Colour", color=colour.value) 
+                    else:
+                        colour = int("0x"+teamcolour,16)
+                        await teamrole.edit(colour=discord.Colour(colour))
+                        embed = discord.Embed(title="Change Team Colour", color=colour) 
                     await message.channel.send("Team colour has been changed.")
                     #Log details
-                    embed = discord.Embed(title="Change Team Colour", color=int("0x"+teamcolour,16)) 
                     embed.add_field(name="Changed By", value=message.author.display_name, inline=False)
                     embed.add_field(name="Changed ID", value=message.author.id, inline=False)
                     await logchannel.send(embed=embed)
