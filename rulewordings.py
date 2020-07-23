6c4c8ac8ab371f7d6c1addc0087d1aac9c8626e1
Thom <larnu@gmail.com>
2020-07-23 12:41:20 +0100

    How Do Throw?

---
+++ rulewordings.py
@@ -0,0 +1,52 @@
+
+throw = ["""***Throw***
+>>> A model making a **throw power attack** picks up and throws another model. A warjack must have at least one non-crippled weapon with the Open Fist weapon quality to make a throw power attack. A warbeast must have at least one weapon with the Open Fist weapon quality to make a throw power attack. A throw power attack has a melee range equal to the melee range of its non-crippled weapon with Open Fist. A model cannot throw power attack a model with a larger base; that is, a model with a larger base cannot be targeted.
+
+The attacking model must first attempt to grasp the model it intends to throw by making a melee attack roll against it. If the attack hits, that model is grasped and tries to break free before it is lifted off the ground. Both models roll a d6 and add their current STR. An attacker with two non-crippled weapons with Open Fist rolls 2d6 instead. If the grasped model’s total is greater, it breaks free and the attack ends. If the attacker’s total equals or exceeds the grasped model’s total, the grasped model is thrown (see “Thrown,” later in these messages).
+
+When your model throws another model, you can choose either to throw the model directly away from the attacker or to throw it at another model within the attacker’s line of sight. The throw distance is equal to half the attacker’s current STR in inches.""",
+""">>> If you choose to have your model throw a model at another model, ignore the model being thrown when determining line of sight to the target. The target can have a larger base than the attacker. If the distance between the thrown model and the model it is being thrown at is beyond the throw distance, the point of impact is the point along the line from the thrown model to the model it is being thrown at equal to the throw distance (see “Thrown,” later in these messages). In this case, do not roll deviation. If the other model is within the throw distance, the attacker makes a melee attack roll against it. On a hit, move the thrown model from its current location directly toward the other model’s base until it contacts the target.
+
+If the target was in the throw range and the attack roll resulted in a miss, determine the thrown model’s actual point of impact by rolling for deviation. Referencing the deviation rules (p. 51 of the Digest Rules), roll a d6 for direction and a d3 for distance in inches. Measure deviation from the center of the missed model’s base. The deviation distance cannot exceed half the distance between the thrown model and the model that was missed.
+
+When moving the thrown model, it moves through the throwing model without contacting it. The thrown model suffers a power attack damage roll.""",
+"""***Thrown***
+>>> Sometimes models are **thrown** as the result of a spell or attack. When a model is thrown, refer to the spell or attack that caused the throw to determine the model’s point of impact, generally directly away from the attacking model. When resolving a throw power attack, however, it may be necessary to roll for deviation to determine the thrown model’s point of impact (see earlier in these messages, and “Deviation,” p. 51 of the Digest Rules). Move the thrown model from its current location in a straight line directly toward the point of impact. During this movement, a thrown model moves through models with smaller bases without contacting them. Unlike when a model is slammed, rough terrain does not affect this movement, but the thrown model still stops if it contacts an obstacle, an obstruction, or a model with an equal or larger base. After moving, the thrown model becomes knocked down (p. 61 of the Digest Rules) and contacts all models with which it is base-to-base and all models whose bases it overlaps. The thrown model then suffers a damage roll determined by the spell or attack that caused the throw.
+
+Add an additional die to the damage roll the thrown model suffers if it contacts an obstacle, an obstruction, or a model with an equal or larger base.""",
+""">>> If a thrown model contacts a model with an equal or smaller base, the contacted model becomes knocked down (p. 61 of the Digest Rules) and suffers a collateral damage roll determined by the spell or attack that caused the throw. A contacted model with a larger base than the thrown model does not suffer collateral damage and is not knocked down. Resolve any collateral damage simultaneously with the damage resulting from the spell or attack that caused the throw (see “Simultaneous Effects,” p. 41 of the Digest Rules).
+
+If a thrown model cannot be knocked down it must still forfeit its Normal Movement or Combat Action if it activates later in a turn in which it was thrown."""]
+
+slam = ["""***Slam***
+>>> A model making a **slam power attack** rams a model with the full force of its body to send the target model flying backward and knock it to the ground. A slam combines a model’s Normal Movement and Combat Action. A model cannot make a slam power attack if it forfeited either its Normal Movement or its Combat Action that activation.
+
+A slam power attack made by a colossal or gargantuan has a 2 ̋ melee range. A slam power attack made by any other model has a 0.5 ̋ melee range.
+
+A model making a slam power attack during its activation can attempt to slam any model that is in its line of sight at the beginning of its Normal Movement. Declare a slam and its target before moving the slamming model. Remember, a model requires line of sight to another model to target it. After declaring a slam, turn the slamming model to directly face the slam target. The slamming model then advances its full SPD plus 3 ̋ directly toward its target. The slamming model cannot voluntarily stop its movement unless its target is in its melee range, but it can end this movement at any point with its target in its slam power attack’s melee range. It stops if it contacts a model, an obstacle, or an obstruction or if it is pushed, slammed, thrown, or placed during its slam movement. If a model contacts a model, an obstacle, or an obstruction while moving as part of a slam power attack but is able to move through it for some reason, the slamming model does not stop but is still considered to have contacted the model, obstacle, or obstruction.""",
+""">>> The slamming model cannot change its facing after advancing as part of a slam.
+
+A slamming model that ends its slam movement with its target in the melee range of its slam power attack has made a successful slam. If the slamming model advanced at least 3 ̋, it makes a melee attack roll against its target. A model that slam power attacks a model with a larger base suffers –2 to its attack roll. If the attack hits, the target is slammed d6 ̋ directly away from the attacker, becomes knocked down, and then suffers a power attack damage roll (see “Slammed,” later in these messages).
+
+If the slamming model has a smaller base than the model it slammed, the slammed model is slammed half the distance rolled. Smaller-based models hit by a slam power attack made by a huge-based model are moved an additional 2 ̋.
+
+If a slamming model makes a successful slam but moved less than 3 ̋, the model still makes an attack roll against its target. If the target is hit, it suffers a power attack damage roll but is not slammed. These are still slam attack rolls and slam damage rolls.
+
+A model that does not end its slam movement with its target within its slam melee range has failed its slam power attack. If a model fails its slam power attack during its activation, its activation ends.""",
+""" >>> ***Slams Outside of Activation***
+
+When a model makes a slam power attack outside its activation, such as with the Counter Slam special rule, follow the rules above but ignore any references to the model’s activation, Normal Movement, or Combat Action. If the model fails its slam, it does not make any attack.""",
+"""***Slammed***
+>>> Sometimes models are slammed as a result of a spell or attack. When a model is slammed, it is moved a certain distance directly away from the point of origin of the slam (usually the attacker), then becomes knocked down (p. 61 of the Digest Rules). The distance the slammed model is moved is determined by the spell or attack that caused the slam. The slammed model then suffers a damage roll determined by the spell or attack that caused the slam. A slammed model moves at half rate through rough terrain, suffers any damaging effects through which it passes, and stops if it contacts an obstacle, an obstruction, or a model with an equal or larger base. A slammed model moves through models with smaller bases than its own.
+
+Add an additional die to the damage roll the slammed model suffers if it contacts an obstacle, an obstruction, or a model with an equal or larger base.
+
+If a slammed model contacts a model with an equal or smaller base or moves through a model with a smaller base, the contacted model becomes knocked down (p. 61 of the Digest Rules) and suffers a collateral damage roll determined by the spell or attack that caused the slam. A contacted model with a larger base does not suffer collateral damage and is not knocked down. Resolve any collateral damage simultaneously with the damage resulting from the spell or attack that caused the slam (see “Simultaneous Effects,” p. 41 of the Digest Rules).
+
+If a slammed model cannot be knocked down it must still forfeit its Normal Movement or Combat Action if it activates later in a turn in which it was slammed."""]
+
+def throwrules():
+    return throw
+
+def slamrules():
+    return slam
